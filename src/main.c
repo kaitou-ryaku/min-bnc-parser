9130afff58e441c1aed660c89e193e8c11f2a8fd
kaitou_ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-10-18 01:40:15 +0900

    正規表現に英数字全体等のメタ文字を追加し、lexerのbnfを整理した

--- src/main.c
+++ src/main.c
@@ -2,39 +2,35 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, const char **argv) {
-  if (argc != 1) {
-    fprintf(stderr, "Invalid argc (%d)\n", argc);
-    return 1;
-  }
+int main(void) {
 
   const char* bnf_str = ""
-    "  number     : (0|(1|2)(0|1|2)*)\n"
-    "  identifier : (a|b)(0|a|b)*\n"
+    "  number     : (0|(1|2|3|4|5|6|7|8|9)\\d*)\n"
+    "  identifier : (\\l|\\u|_)(\\l|\\u|_|\\d)*\n"
     "  plusminus  : (+|-)\n"
     "  starslash  : (\\*|/)\n"
     "  lparen     : \\(\n"
     "  rparen     : \\)\n"
   ;
 
-  const char* src_str = "(11+20)*201-(21-1)/(0-1*a0b+b)";
+  const char* src_str = "(15+20)*203-(42-0)/(0-7*A0b+b)";
 
-  LEX_BNF        lex[1000];
-  char           name[1000];
-  char           bnf[1000];
-  char           simple[1000];
-  MIN_REGEX_NODE node[1000];
+  LEX_BNF        lex[5000];
+  char           name[5000];
+  char           bnf[5000];
+  char           simple[5000];
+  MIN_REGEX_NODE node[5000];
 
-  const int lex_max_size    = 1000;
-  const int name_max_size   = 1000;
-  const int bnf_max_size    = 1000;
-  const int simple_max_size = 1000;
-  const int node_max_size   = 1000;
+  const int lex_max_size    = 5000;
+  const int name_max_size   = 5000;
+  const int bnf_max_size    = 5000;
+  const int simple_max_size = 5000;
+  const int node_max_size   = 5000;
 
   const int lex_size = create_lexer(bnf_str, lex, lex_max_size, name, name_max_size, bnf, bnf_max_size, simple, simple_max_size, node, node_max_size);
 
-  LEX_TOKEN token[100];
-  const int token_max_size = 100;
+  LEX_TOKEN token[1000];
+  const int token_max_size = 1000;
 
   const int token_size = match_lexer(token, token_max_size, lex, lex_size, src_str);
 
