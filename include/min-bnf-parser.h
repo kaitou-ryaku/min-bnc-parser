630a8d6aa26348e1b976ef1a7fbd12848e0c5a4f
kaitou_ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-10-31 03:40:45 +0900

    コンパイラ作成用に、一括で*.hをインクルードできるmin-bnc-parser.hを追加

---
+++ include/min-bnf-parser.h
@@ -0,0 +1,10 @@
+#ifndef __MIN_BNC_PARSER__
+#define __MIN_BNC_PARSER__
+
+#include "../include/bnf.h"
+#include "../include/lexer.h"
+#include "../include/syntax.h"
+#include "../include/parser.h"
+#include "../include/pair_bnf.h"
+
+#endif
